462e232c4ec49f04ad3d42090e6dd6019de6d670
D <2480326@cognizant.com>
2026-03-25 14:36:07 +0530

    Initial commit

---
+++ src/main/java/com/bedmaster/BedmasterApplication.java
@@ -0,0 +1,14 @@
+package com.bedmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedmasterApplication.class, args);
+		System.out.println("Leooooooo");
+	}
+
+}
---
+++ src/test/java/com/bedmaster/BedMasterApplication.java
@@ -0,0 +1,13 @@
+package com.bedmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedMasterApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(BedMasterApplication.class, args);
+        System.out.println("Leooooooo");
+    }
+}
